8da4ec471f5a52d6d66b7f2e56a634d15b256354
Nick Cross <njcross1990@gmail.com>
2025-04-30 07:25:17 -0500

    remove header

--- backend/app/schemas/cart_schema.py
+++ backend/app/schemas/cart_schema.py
@@ -4,7 +4,7 @@ from app.schemas.kit_schema import KitSchema
 class CartSchema(Schema):
     id = fields.Int(dump_only=True)
     user_id = fields.Int(required=True)
-    character_id = fields.Int(required=True)
+    kit_id = fields.Int(required=True)
     quantity = fields.Int(required=True)
     character = fields.Nested(KitSchema, dump_only=True)
 
