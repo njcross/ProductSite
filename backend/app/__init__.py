c7861daf7cb1528d93a1066124b6b46afcbf14c9
Nick Cross <njcross1990@gmail.com>
2025-05-04 14:07:07 -0500

    sso fix

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -105,7 +105,7 @@ def create_app():
             user = User(
                 username=username,
                 email=email,
-                password=None,
+                password="",
                 role='user',
                 oauth_provider='google'
             )
