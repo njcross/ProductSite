f4ce0e0daae5a83d256efb8945893d9d41e6c8e9
Nick Cross <njcross1990@gmail.com>
2025-05-06 07:20:21 -0500

    make a new coordinates field for inventory

--- backend/app/config.py
+++ backend/app/config.py
@@ -7,7 +7,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     ROOT_DATABASE_URL = os.getenv('ROOT_DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    CORS_ORIGIN = os.getenv('CORS_ORIGIN')
+    CORS_ORIGIN = [os.getenv('CORS_ORIGIN1'), os.getenv('CORS_ORIGIN2')]
     GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
     GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET')
     OAUTHLIB_INSECURE_TRANSPORT = True  # Only for local dev
