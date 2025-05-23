a26153889dadaea8da773e96999f2579dfacee9a
Nick Cross <njcross1990@gmail.com>
2025-05-23 14:46:42 -0500

    fixes

--- react-router-bootstrap-app/src/components/CharacterList.jsx
+++ react-router-bootstrap-app/src/components/CharacterList.jsx
@@ -57,7 +57,7 @@ export default function CharacterList({
     } catch (err) {
       console.error('Failed to load characters:', err);
     }
-  }, [API_BASE, sortBy, sortDir, page, itemsPerPage, search
+  }, [API_BASE, sortBy, sortDir, page, itemsPerPage, search, selectedAges, selectedCategories, rating, locations
 
   ]);
   
