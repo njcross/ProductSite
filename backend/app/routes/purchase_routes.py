156e5c840bced56ec4b15aff0d851cccef8b279b
Nick Cross <njcross1990@gmail.com>
2025-05-23 17:13:48 -0500

    ability to chose from existing inventory names

--- backend/app/routes/purchase_routes.py
+++ backend/app/routes/purchase_routes.py
@@ -23,7 +23,7 @@ def create_purchase():
         inventory_id=data.get('inventory_id'),
         payment_method=data.get('payment_method'),
         available_date="1234",
-        pick_up_date=datetime.now(datetime.timezone.utc + timedelta(hours=24)
+        pick_up_date=datetime.now(datetime.timezone.utc) + timedelta(hours=24)
     )
 
     db.session.add(new_purchase)
