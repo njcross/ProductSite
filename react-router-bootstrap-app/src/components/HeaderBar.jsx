68b4e2070985ce9fa3af2688f7217b87150d64e4
Auto <deploy@myplaytray.com>
2025-06-06 22:36:24 -0500

    styling

--- react-router-bootstrap-app/src/components/HeaderBar.jsx
+++ react-router-bootstrap-app/src/components/HeaderBar.jsx
@@ -48,7 +48,7 @@ export default function HeaderBar() {
         <label><EditableField contentKey="content_253" /> {/* "Search our trays" */}</label>
         <input
           type="text"
-          placeholder="Search Trays..."
+          placeholder="Search Play Trays..."
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
